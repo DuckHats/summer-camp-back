3089b52ec89aca3b2d0f277d8e63721c5616f1b9
Gerijacki <gerardloriz05@gmail.com>
2025-04-14 15:43:27 +0200

    feat: Add email notification for users during bulk creation process; update dependencies in composer files

--- app/Jobs/BulkUserCreationJob.php
+++ app/Jobs/BulkUserCreationJob.php
@@ -12,7 +12,9 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
-
+use App\Helpers\EmailHelper;
+use App\Mail\PasswordChangedMail;
+use App\Mail\WelcomeMail;
 class BulkUserCreationJob implements ShouldQueue
 {
     // Per executar el worker: php artisan queue:work --queue=bulk-processing,user-creation
@@ -53,6 +55,11 @@ class BulkUserCreationJob implements ShouldQueue
 
             DB::commit();
             Log::info('Bulk user creation completed successfully.');
+            Log::info('Let send the email to the user');
+            foreach ($this->users as $userData) {
+                Log::info('Send email to user: ' . $userData['email']);
+                EmailHelper::sendEmail($user->email, WelcomeMail::class, [$user]);
+            }
 
         } catch (\Throwable $e) {
             DB::rollBack();
